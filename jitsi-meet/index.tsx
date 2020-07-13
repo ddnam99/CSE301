b2e770089e35a7272cf8dab7c29e68f16e4192b1
Double D <nampt1999@gmail.com>
2020-07-13 11:37:28 +0700

    use .env

--- jitsi-meet/index.tsx
+++ jitsi-meet/index.tsx
@@ -2,10 +2,13 @@ import "react-app-polyfill/ie11";
 import React from "react";
 import * as ReactDOM from "react-dom";
 import { Provider } from "react-redux";
+import dotenv from "dotenv";
 
 import App from "./src/App";
 
 import store from "./src/redux/store";
+
+dotenv.config();
 
 ReactDOM.render(
   <Provider store={store}>
--- jitsi-meet/src/helpers/Interceptors.ts
+++ jitsi-meet/src/helpers/Interceptors.ts
@@ -1,11 +1,12 @@
 import axios from "axios";
+import process from "process";
 
 axios.interceptors.request.use(
   (config) => {
     const token = localStorage.token;
     if (token) config.headers.authorization = JSON.parse(token);
 
-    config.baseURL = "http://localhost:4000";
+    config.baseURL = process.env.JITSI_API || "http://localhost:4000";
 
     return config;
   },
